7e762c8554c473527cc690ae255e53bbf5289305
Troy Ong <troy.ong@hoffi.com>
2026-06-04 15:11:48 +0100

    updated AOS library integration to do it locally instead of cdn to improve delivery times

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import { Navbar } from "@layout/Navbar";
 
 import { Hero } from "@sections/Hero";
@@ -7,7 +9,15 @@ import { Experience } from "@sections/Experience";
 import { Projects } from "@sections/Projects";
 import { Contact } from "@sections/Contact";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 export default function App() {
+  useEffect(() => {
+    AOS.init({ duration: 500 });
+    AOS.refresh();
+  }, []);
+
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Navbar />
