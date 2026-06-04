af87421df99383a7f6e5c53bea40200b4c7b0f28
Troy Ong <troy.ong@hoffi.com>
2026-06-04 14:21:20 +0100

    changed image formats to webp to reduce size and improve load performance

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = () => {
       {/* Bg */}
       <div className="absolute inset-0">
         <img
-          src="/hero-bg.jpg"
+          src="/hero-bg.webp"
           alt="hero image"
           className="w-full h-full object-cover opacity-40"
         />
