c8df0e7d765b44eda9a6ff7aff6a51242f8a1853
Troy Ong <troy.ong@hoffi.com>
2026-06-07 23:10:58 +0100

    removed fade-in animation for footer as its causing overflow

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,6 @@
 export const Footer = () => {
   return (
-    <footer
-      data-aos="fade-up"
-      className="footer text-xs font-mono text-center p-4 overflow-hidden"
-    >
+    <footer className="footer text-xs font-mono text-center p-4 overflow-hidden">
       <p>&copy; {new Date().getFullYear()} Troy Ong. All rights reserved.</p>
       <p>
         Built with{" "}
