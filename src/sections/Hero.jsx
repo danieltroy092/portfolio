d30f4d2dba7e7dc58a5fe3db29b147609d586399
Troy Ong <troy.ong@hoffi.com>
2026-06-07 15:54:41 +0100

    fixing button-link hover state

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -69,7 +69,7 @@ export const Hero = () => {
         <ButtonLink
           data-aos="fade-up"
           data-aos-delay="1000"
-          className="inline-block mt-8 py-2 px-6 font-semibold gradient-button bordered"
+          className="inline-block mt-8 py-2 px-6 hover:text-background gradient-button bordered"
           href="#contact"
         >
           Get in Touch
